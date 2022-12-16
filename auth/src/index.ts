08098a921b06fe57c692760ab3564e4c9a693754
rawwr <akshayace0302@gmail.com>
2022-12-17 01:02:07 +0530

    toJSON and jwt

--- auth/src/index.ts
+++ auth/src/index.ts
@@ -3,6 +3,7 @@ import "express-async-errors";
 import { json } from "body-parser";
 
 import mongoose from "mongoose";
+import cookieSession from "cookie-session";
 
 import { currentUserRouter } from "./routes/current-user";
 import { signinRouter } from "./routes/signin";
@@ -12,7 +13,16 @@ import { errorHandler } from "./middlewares/error-handler";
 import { NotFoundError } from "./errors/not-found-errors";
 
 const app = express();
+app.set("trust proxy", true);
+
 app.use(json());
+
+app.use(
+  cookieSession({
+    signed: false,
+    secure: true,
+  })
+);
 
 app.use(currentUserRouter);
 app.use(signinRouter);
@@ -26,6 +36,10 @@ app.all("*", async (req, res) => {
 app.use(errorHandler);
 
 const start = async () => {
+  if (!process.env.JWT_KEY) {
+    throw new Error("JWT_KEY must be defined");
+  }
+
   mongoose.set("strictQuery", true);
 
   try {
