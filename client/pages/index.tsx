6ef96a67ce63e033e3677db42990e0f147739f4a
rawwr <akshayace0302@gmail.com>
2022-12-24 01:24:57 +0530

    getting current user

--- client/pages/index.tsx
+++ client/pages/index.tsx
@@ -1,9 +1,22 @@
-const homepage = () => {
-  return (
-    <div>
-      <h1>Homepage</h1>
-    </div>
+import buildClient from "../helpers/build-client";
+
+const homepage = ({ currentUser }: any | null) => {
+  console.log(currentUser, "current user");
+  return currentUser ? (
+    <h1>You are signed in</h1>
+  ) : (
+    <h1>You are NOT signed in</h1>
   );
 };
 
+export const getServerSideProps = async (context: any) => {
+  // console.log(req.headers);
+  // requests should be made to http://ingress-nginx.ingress-nginx...laksdjfk
+  const client = buildClient(context);
+  const { data } = await client.get("/api/users/currentuser");
+  // console.log(data);
+  return {
+    props: data,
+  };
+};
 export default homepage;
