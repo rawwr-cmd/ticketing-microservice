e8f19ea03db9f00e8af85e8b7051de6dfb9bd44d
rawwr <akshayace0302@gmail.com>
2022-12-26 02:38:29 +0530

    adding build route for getInitialProps

--- client/helpers/build-client.tsx
+++ client/helpers/build-client.tsx
@@ -2,9 +2,17 @@ import axios from "axios";
 
 const buildClient = ({ req }: any) => {
   // we are on the server
-  return axios.create({
-    baseURL: "http://ingress-nginx-controller.ingress-nginx.svc.cluster.local",
-    headers: req.headers,
-  });
+  if (typeof window === "undefined") {
+    return axios.create({
+      baseURL:
+        "http://ingress-nginx-controller.ingress-nginx.svc.cluster.local",
+      headers: req.headers,
+    });
+  } else {
+    // we must be on the browser
+    return axios.create({
+      baseURL: "/",
+    });
+  }
 };
 export default buildClient;
