1ec6fe41e69cef968e78f47e7a7d275fbd97a0b2
rawwr <akshayace0302@gmail.com>
2022-12-26 23:43:57 +0530

    additional config

---
+++ NpmSharedModules/src/index.ts
@@ -0,0 +1,15 @@
+interface COLOR {
+  red: string;
+  green: string;
+  blue: string;
+}
+
+const Color: COLOR = {
+  red: "1002",
+  green: "092",
+  blue: "100",
+};
+
+console.log(Color);
+
+export default Color;
