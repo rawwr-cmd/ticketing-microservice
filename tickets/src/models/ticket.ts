075f16a864f19e7b59252a76c2bde7cde3826575
rawwr <akshayace0302@gmail.com>
2022-12-29 02:00:10 +0530

    ticket model

---
+++ tickets/src/models/ticket.ts
@@ -0,0 +1,52 @@
+import mongoose, { Schema, model } from "mongoose";
+
+interface TicketAttrs {
+  title: string;
+  price: number;
+  userId: string;
+}
+
+// an interface that describes the properties
+// that a Ticket Model has
+interface TicketDoc extends mongoose.Document {
+  title: string;
+  price: number;
+  userId: string;
+}
+
+interface TicketModel extends mongoose.Model<TicketDoc> {
+  build(attrs: TicketAttrs): TicketDoc;
+}
+
+const ticketSchema = new Schema(
+  {
+    title: {
+      type: String,
+      required: true,
+    },
+    price: {
+      type: Number,
+      required: true,
+    },
+    userId: {
+      type: String,
+      required: true,
+    },
+  },
+  {
+    toJSON: {
+      transform(doc, ret) {
+        ret.id = ret._id;
+        delete ret._id;
+      },
+    },
+  }
+);
+
+ticketSchema.statics.build = (attrs: TicketAttrs) => {
+  return new Ticket(attrs);
+};
+
+const Ticket = model<TicketDoc, TicketModel>("Ticket", ticketSchema);
+
+export { Ticket };
