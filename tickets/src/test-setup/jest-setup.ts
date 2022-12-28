378933c6a9fe193b6ce9d446dc7169c272097d20
rawwr <akshayace0302@gmail.com>
2022-12-28 23:45:36 +0530

    building a session for fake auth

--- tickets/src/test-setup/jest-setup.ts
+++ tickets/src/test-setup/jest-setup.ts
@@ -1,5 +1,9 @@
 import { MongoMemoryServer } from "mongodb-memory-server";
 import mongoose from "mongoose";
+import jwt from "jsonwebtoken";
+declare global {
+  var signin: () => string[];
+}
 
 let mongo: any;
 mongoose.set("strictQuery", false);
@@ -23,3 +27,26 @@ afterAll(async () => {
   await mongo.stop();
   await mongoose.connection.close();
 });
+
+global.signin = () => {
+  // Build a JWT payload. { id, email }
+  const payload = {
+    id: "wuigdugwud",
+    email: "test@gmail.com",
+  };
+
+  // Create the JWT!
+  const token = jwt.sign(payload, process.env.JWT_KEY!);
+
+  // Build session Object { jwt: MY_JWT }
+  const session = { jwt: token };
+
+  // Turn that session into JSON
+  const sessionJSON = JSON.stringify(session);
+
+  // Take JSON and encode it as base64
+  const base64 = Buffer.from(sessionJSON).toString("base64");
+
+  // Return a string thats the cookie with the encoded data
+  return [`session=${base64}`];
+};
