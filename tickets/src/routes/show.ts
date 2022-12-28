9967e459ac920b3eb835d2df73398f0e4e6e5045
rawwr <akshayace0302@gmail.com>
2022-12-29 02:43:57 +0530

    has to fix an error

---
+++ tickets/src/routes/show.ts
@@ -0,0 +1,26 @@
+import express, { Request, Response } from "express";
+import {
+  requireAuth,
+  validateRequest,
+  NotFoundError,
+  NotAuthorizedError,
+} from "@rawtickets/common";
+import { Ticket } from "../models/ticket";
+
+const router = express.Router();
+
+router.get(
+  "/api/tickets/:id",
+  requireAuth,
+  async (req: Request, res: Response) => {
+    const ticket = await Ticket.findById(req.params.id);
+
+    if (!ticket) {
+      throw new NotFoundError();
+    }
+
+    res.send(ticket);
+  }
+);
+
+export { router as showTicketRouter };
