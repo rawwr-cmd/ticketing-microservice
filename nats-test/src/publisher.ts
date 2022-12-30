f2af389bc60eeaa4ce9c07af57ffe9595a8df9cd
rawwr <akshayace0302@gmail.com>
2022-12-31 01:21:04 +0530

    publishing events

---
+++ nats-test/src/publisher.ts
@@ -0,0 +1,20 @@
+import { statSync } from "fs";
+import nats from "node-nats-streaming";
+
+const stan = nats.connect("ticketing", "abc", {
+  url: "http://localhost:4222",
+});
+
+stan.on("connect", () => {
+  console.log("Publisher connected to nats");
+
+  const data = JSON.stringify({
+    id: "123",
+    title: "concert",
+    price: 20,
+  });
+
+  stan.publish("ticket:created", data, () => {
+    console.log("Event published");
+  });
+});
